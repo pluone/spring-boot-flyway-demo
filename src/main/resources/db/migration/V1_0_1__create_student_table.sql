9a37599fa841e450d7117e8ae7cf352ab6af8781
gitlab admin <root@example.gitlab.com>
2017-06-16 06:40:24 +0800

    flyway demo

---
+++ src/main/resources/db/migration/V1_0_1__create_student_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `student` (
+  `student_id` int(11) NOT NULL,
+  `student_name` varchar(45) DEFAULT NULL,
+  `student_sex` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
---
+++ src/main/resources/db/migration/V1_0_2__insert_data_into_student.sql
@@ -0,0 +1,1 @@
+INSERT INTO `test_db`.`student` (`student_id`, `student_name`, `student_sex`) VALUES ('1', 'Frank', '1');
